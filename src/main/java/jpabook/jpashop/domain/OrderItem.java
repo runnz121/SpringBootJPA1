c67813712d13339222e81be9f2ada8e94ba6bd68
runnz121 <runnz121@gmail.com>
2020-12-01 17:50:25 +0900

    add orderrepository

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,6 +4,7 @@ package jpabook.jpashop.domain;
 import jpabook.jpashop.domain.Item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -28,5 +29,26 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count;  //주문 수량
+
+    //==생성 메소드 ==/
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직 ==//
+    public void cancel(){
+        getItem().addStock(count); //재고수량 원복
+
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice()*getCount();
+    }
 
 }
